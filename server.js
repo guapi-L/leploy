49ec3e53b5192c158493631a181330d126eeb8ea
qovciu <372903575@qq.com>
2015-07-06 01:19:57 +0800

    use child_process to view

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ var http = require('http');
 
 var proxy = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('ok3ay11111122223223322222211111111');
+  res.end('ok3ay1aa1111111122223223322222211111111');
 });
 
-proxy.listen(10071);
+proxy.listen(10071, function(err) {
+  if(err) proxy.listen(10071);
+});
 console.log('server run');
--- server/leploy.js
+++ server/leploy.js
@@ -1,6 +1,8 @@
 var net = require('net');
 var fs = require('fs');
 var path = require('path');
+var child_process = require('child_process')
+var child;
 // createServer
 net.createServer(ctx).listen(10070, function() { 
   console.log('server bound');
@@ -19,8 +21,10 @@ function ctx(sock) { //'connection' listener
     console.log(data);
     fs.writeFile(serverFile, data, function (err) {
       if (err) throw err;
-      delete require.cache[path.join(__dirname , serverFile)];
-      require(serverFile);
+      // delete require.cache[path.join(__dirname , serverFile)];
+      // require(serverFile);
+      if(child) child.kill();
+      child = child_process.fork(serverFile);
       data = '';
     });
     sock.write('finish');

51d953cf05d305337c64c913c1eafc975852b7cd
qovciu <372903575@qq.com>
2015-07-06 16:37:44 +0800

    clear

--- server/cache_Mon_Jul_06_2015/server.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-
-var proxy = http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('try');
-});
-
-proxy.listen(10071);
-console.log('server run');
